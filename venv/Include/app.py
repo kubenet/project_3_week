d9dc20f6c5c01fcf815bdff170ff2c9b091051ab
kubenet <kubenet@gmail.com>
2020-07-17 17:47:22 +0700

    add files templates and data.py

--- venv/Include/app.py
+++ venv/Include/app.py
@@ -3,29 +3,12 @@ from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 
-SECRET_KEY = os.urandom(32)
 app = Flask(__name__)
-app.config['SECRET_KEY'] = SECRET_KEY
-
-
-
-
-# Форма аутентификации
-
-class AuthForm(FlaskForm):
-    login = StringField('Ваш логин')
-    passwd = PasswordField('Ваш пароль')
-    submit = SubmitField('Вход в систему')
 
 
 @app.route('/')
 def index():
-    form = AuthForm()
-
-    login = form.login.data
-    passwd = form.passwd.data
-
-    return render_template("index.html", form=form)
+    return render_template("index.html")
 
 
 app.run('0.0.0.0', debug=True)
