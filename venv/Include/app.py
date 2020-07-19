6ef03f4131ba6a6db36e5cea85e5363f9e36135f
kubenet <kubenet@gmail.com>
2020-07-20 00:51:40 +0700

    add functions update_data() and update_timtale_teacher() and add_request()

--- venv/Include/app.py
+++ venv/Include/app.py
@@ -5,9 +5,44 @@ from wtforms import RadioField, TextField
 
 app = Flask(__name__)
 app.secret_key = 'key'
+
 with open('data.json', 'r') as r:
     all_data = json.load(r)
     r.close()
+
+
+# Запись нового запроса в файл all_requests.json
+def add_request(name, phone, goal, times):
+    with open('all_requests.json', 'r') as r:
+        records = json.load(r)
+    records.append({'name': name, 'phone': phone, 'goal': goal, 'times': times})
+    r.close()
+    with open('all_requests.json', 'w') as w:
+        json.dump(records, w)
+    w.close()
+
+
+# Запись нового запроса в файл all_requests.json
+def update_timtale_teacher(id_teacher, day, times):
+    with open('data.json', 'r') as r:
+        records = json.load(r)
+        records[1][int(id_teacher)]['free'][day][times] = False
+        r.close()
+
+    with open('data.json', 'w') as w:
+        json.dump(records, w)
+        w.close()
+    with open('data.json', 'r') as r:
+        global all_data
+        all_data = json.load(r)
+        r.close()
+
+
+def update_data():
+    with open('data.json', 'r') as r:
+        global all_data
+        all_data = json.load(r)
+        r.close()
     # for day in all_data[1][0]['free']:
     #     print(day)
     #     for key in all_data[1][0]['free'][day]:
@@ -15,7 +50,10 @@ with open('data.json', 'r') as r:
     #             print(key)
 
 
-class RequestForm(FlaskForm):   # объявление класса формы для WTForms
+update_data()
+
+
+class RequestForm(FlaskForm):  # объявление класса формы для WTForms
     name = TextField('name')
     phone = TextField('phone')
     goal = RadioField("Какая цель занятий?", choices=[('0', 'Для путешествий'), ('1', 'Для школы'), ('2', 'Для работы'),
@@ -53,15 +91,19 @@ def requests():
 
 @app.route('/request_done/', methods=['POST'])  # заявка на подбор отправлена
 def request_done():
-
     form = RequestForm()
     name = form.name.data
     phone = form.phone.data
-    goal  = form.goal.data
+    goal = form.goal.data
     times = form.time.data
 
-    print(name, phone, goal, times)
-    return render_template("request_done.html")
+    goal_choices = {'0': 'Для путешествий', '1': 'Для школы', '2': 'Для работы', '3': 'Для переезда'}
+    time_choices = {'0': '1-2 часа в неделю', '1': '3-5 часов в неделю', '2': '5-7 часов в неделю',
+                    '3': '7-10 часов в неделю'}
+
+    add_request(name, phone, goal_choices[goal], time_choices[times])
+    return render_template("request_done.html", username=name, userphone=phone, goal=goal_choices[goal],
+                           time=time_choices[times])
 
 
 @app.route('/booking/<int:id_techers>/<day>/<time>/')  # здесь будет форма бронирования <id учителя>
@@ -76,6 +118,10 @@ def booking_done():
     clientTeacher = request.form["clientTeacher"]
     clientName = request.form["clientName"]
     clientPhone = request.form["clientPhone"]
+
+    update_timtale_teacher(clientTeacher, clientWeekday,
+                           clientTime)  # Обновляем расписание свободного времени репетитора
+    update_data()
     return render_template("booking_done.html", clientName=clientName, clientPhone=clientPhone, clientTime=clientTime,
                            clientTeacher=clientTeacher, clientWeekday=clientWeekday)
 
--- venv/Include/read_data.py
+++ venv/Include/read_data.py
@@ -9,8 +9,4 @@ with open("data.json", "w") as f:
    json.dump(lst, f)
 f.close
 
-#
-# f = open('data.json', 'r')
-# teachers = json.loads(f.read())
-# f.close
 
